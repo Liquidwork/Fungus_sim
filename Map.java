f5affc031beee429e916ebe81de6665fa411157f
Liquidwork <52688110+Liquidwork@users.noreply.github.com>
2021-02-06 17:53:01 +0800

    *KEY METHOD* Spreading method complete
    Used queue to implement the spreading.

--- Map.java
+++ Map.java
@@ -1,10 +1,14 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * This is a map that conducts the simulation of fungi spreading model.
  */
 
 public class Map {
-    Fungus[][] map;
-    Climate climate;
+    public Fungus[][] map;
+    public Climate climate;
+    private Queue<int[]> spreadable = new LinkedList<>(); //A queue to implement the spreading model
     /**
      * Initialize a map with a preset size
      * @param size the size of the map
@@ -26,36 +30,59 @@ public class Map {
                 if(map[i][j] == null) continue;
                 map[i][j].grow(climate);
                 if(map[i][j].spreadProgress >= 1){
-                    this.spreadCell(i, j);
-                    map[i][j].spreadProgress -= 1.;
+                    int[] pos = {i, j};
+                    spreadable.offer(pos); // Record the spreadable cells
                 }
             }
+        }
+        while(!spreadable.isEmpty()){ //Spread all the cells which is spreadable, the queue may extend itself while spreading
+            int[] pos = spreadable.poll();
+            spreadCell(pos[0], pos[1]);
         }
     }
 
     /**
      * Spread a cell if possible
+     * It may offer several new elements into the spreadable queue if the new cell is still spreadable
+     * @param i cell coordinate i
+     * @param j cell coordinate j
      */
     private void spreadCell(int i, int j){
-        int savedProcess = (int)map[i][j].spreadProgress - 1;
-        if(i != 0){
-            this.map[i-1][j] = this.map[i-1][j] == null ? (Fungus)this.map[i][j].clone() :  this.map[i-1][j];
-            
+        double savedProcess = (int)map[i][j].spreadProgress - 1;
+        map[i][j].spreadProgress = 0; map[i][j].spreaded = true;
+        if(i != 0 && this.map[i-1][j] == null){
+            this.fungiClone(i, j, i-1, j, savedProcess);
         }
-        if(j != 0){
-            this.map[i][j-1] = this.map[i][j-1] == null ? (Fungus)this.map[i][j].clone() :  this.map[i][j-1];
+        if(j != 0 && this.map[i][j-1] == null){
+            this.fungiClone(i, j, i, j-1, savedProcess);
         }
-        if(i != 0 && j != 0){
-            this.map[i-1][j-1] = this.map[i-1][j-1] == null ? (Fungus)this.map[i][j].clone() :  this.map[i-1][j-1];
+        if(i != 0 && j != 0 && this.map[i-1][j-1] == null){
+            this.fungiClone(i, j, i-1, j-1, savedProcess);
         }
-        if(i != map.length-1){
-            this.map[i+1][j] = this.map[i+1][j] == null ? (Fungus)this.map[i][j].clone() :  this.map[i+1][j];
+        if(i != map.length-1 && this.map[i+1][j] == null){
+            this.fungiClone(i, j, i+1, j, savedProcess);
         }
-        if(j != map[0].length-1){
-            this.map[i][j+1] = this.map[i][j+1] == null ? (Fungus)this.map[i][j].clone() :  this.map[i][j+1];
+        if(j != map[0].length-1 && this.map[i][j+1] == null){
+            this.fungiClone(i, j, i, j+1, savedProcess);
         }
-        if(i != map.length-1 && j != map[0].length-1){
-            this.map[i+1][j+1] = this.map[i+1][j+1] == null ? (Fungus)this.map[i][j].clone() :  this.map[i+1][j+1];
+        if(i != map.length-1 && j != map[0].length && this.map[i+1][j+1] == null){
+            this.fungiClone(i, j, i+1, j+1, savedProcess);
+        }
+    }
+    /**
+     * A method called in spreadCell
+     * @param i coordinate i being cloned
+     * @param j coordinate j being cloned
+     * @param newI coordinate i to be cloned into
+     * @param newJ coordinate j to be cloned into
+     * @param savedProcess a saved 
+     */
+    private void fungiClone(int i, int j, int newI, int newJ, double savedProcess){
+        this.map[newI][newJ] = (Fungus)this.map[i][j].clone();
+        this.map[newI][newJ].spreadProgress = savedProcess;
+        if(savedProcess >= 1){  // If the cell is still spreadable
+            int[] pos = {newI, newJ};
+            spreadable.offer(pos); // Add new spreadable into the queue
         }
     }
 }
